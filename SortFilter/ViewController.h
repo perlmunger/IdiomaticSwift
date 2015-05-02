284b961b37ec4b622c0fce70d409bd994660a516
Matt Long <mlong@Browncoat.local>
2015-05-02 10:19:42 -0600

    Initial Commit

---
+++ SortFilter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SortFilter
+//
+//  Created by Matt Long on 5/2/15.
+//  Copyright (c) 2015 SkyeMark, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SortFilter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SortFilter
+//
+//  Created by Matt Long on 5/2/15.
+//  Copyright (c) 2015 SkyeMark, LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
